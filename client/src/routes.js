04db23bf8301ec10df94ebab24f4e4f1171b1452
Евгений Горин <evg_gorin@mail.ru>
2024-10-23 22:57:57 +0400

    Added Routes

---
+++ client/src/routes.js
@@ -0,0 +1,41 @@
+import { Component } from "react";
+import Admin from "./pages/Admin";
+import { ADMIN_ROUTE, BASKET_ROUTE, ITEM_ROUTE, LOGIN_ROUTE, REGISTRATION_ROUTE, SHOP_ROUTE } from "./utils/consts";
+import Auth from "./pages/Auth";
+import Basket from "./pages/Basket";
+import Shop from "./pages/Store";
+import ItemPage from "./pages/ItemPage";
+
+export const authRoutes = [
+  {
+    path: ADMIN_ROUTE,
+    Component: Admin
+  },
+  {
+    path: BASKET_ROUTE,
+    Component: Basket
+  }
+];
+
+export const publicRoutes = [
+  {
+    path: SHOP_ROUTE,
+    Component: Shop
+  },
+  {
+    path: LOGIN_ROUTE,
+    Component: Auth
+  },
+  {
+    path: REGISTRATION_ROUTE,
+    Component: Auth
+  },
+  {
+    path: BASKET_ROUTE,
+    Component: Basket
+  },
+  {
+    path: ITEM_ROUTE + '/:id',
+    Component: ItemPage
+  }
+];
---
+++ client/src/utils/consts.js
@@ -0,0 +1,6 @@
+export const ADMIN_ROUTE = '/admin';
+export const LOGIN_ROUTE = '/auth/login';
+export const REGISTRATION_ROUTE = '/auth/registration';
+export const BASKET_ROUTE = '/basket';
+export const ITEM_ROUTE = '/item';
+export const SHOP_ROUTE = '/';
