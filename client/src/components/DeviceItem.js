f2b7bf364af0f464327350ef1963379d805bcafe
Евгений Горин <evg_gorin@mail.ru>
2024-10-30 23:55:25 +0400

    Added DeviceItem.

--- client/src/components/DeviceItem.js
+++ client/src/components/DeviceItem.js
@@ -3,14 +3,15 @@ import Card from 'react-bootstrap/Card'
 import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
 import { useNavigate } from "react-router-dom"
+import { ITEM_ROUTE } from "../utils/consts";
 
 const DeviceItem = ({item})  => {
 
-  const history = useNavigate()
-  console.log(history)
+  const navigate = useNavigate()
+  console.log(navigate)
 
   return (
-    <Col md={3} className={"mt-3"}>
+    <Col md={3} className={"mt-3"} onClick={() => navigate(ITEM_ROUTE + '/' + item.id)}>
       <Card style={{width: 150, cursor: "pointer"}}>
         <Image width={150} height={150} src={item.immage}/>
         <div className="text-black-50 d-flex justify-content-between align-items-center">
--- client/src/pages/ItemPage.js
+++ client/src/pages/ItemPage.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { Col, Container, Image } from "react-bootstrap";
 
 const ItemPage = () => {
+
+  const item = {id: 1, title: 'Обруч 90 см', price: 1200, image: ''};
+
   return (
-    <div>
-      ITEMPAGE
-    </div>
+    <Container>
+      <Col md={4}>
+        <Image width={300} height={300} src={item.image}/>
+      </Col>
+      <Col md={4}>
+
+      </Col>
+      <Col md={4}>
+
+      </Col>
+    </Container>
   );
 }
 
